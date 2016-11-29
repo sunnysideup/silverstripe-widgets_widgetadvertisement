a2005f686db313ead50be21ec9ba7c9c9c392734
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:04:35 +1300

    PATCH: module clean-up

--- WidgetAdvertisement.php
+++ WidgetAdvertisement.php
@@ -2,35 +2,36 @@
 /**
  *@author nicolaas [at] sunnysideup.co.nz
  */
-class WidgetAdvertisement extends Widget {
+class WidgetAdvertisement extends Widget
+{
+    private static $db = array(
+        "Hyperlink" => "Varchar(255)"
+    );
 
-	private static $db = array(
-		"Hyperlink" => "Varchar(255)"
-	);
+    private static $has_one = array(
+        "Logo" => "Image"
+    );
 
-	private static $has_one = array(
-		"Logo" => "Image"
-	);
+    private static $cmsTitle = 'Advertisement';
 
-	private static $cmsTitle = 'Advertisement';
+    private static $description = 'Adds a simple image + link (advertisement) to your widget area.';
 
-	private static $description = 'Adds a simple image + link (advertisement) to your widget area.';
+    public function getCMSFields()
+    {
+        $fields = new FieldList();
+        $fields->push(new TextField("Hyperlink", "Hyperlink (e.g. http://www.silverstripe.co.nz)"));
+        $fields->push(new HeaderField("MakeSureToUploadImageFirst", "Please make sure image is uploaded first in file and images section", 5));
+        $fields->push(new DropdownField("LogoID", "Image / Picture / Logo", array(0 => "--- please select ---") + Image::get()->map()->toArray()));
+        return $fields;
+    }
 
-	function getCMSFields() {
-		$fields = new FieldList();
-		$fields->push(new TextField("Hyperlink","Hyperlink (e.g. http://www.silverstripe.co.nz)"));
-		$fields->push(new HeaderField("MakeSureToUploadImageFirst","Please make sure image is uploaded first in file and images section", 5));
-		$fields->push(new DropdownField("LogoID","Image / Picture / Logo", array(0 => "--- please select ---") + Image::get()->map()->toArray()));
-		return $fields;
-	}
-
-	function WidgetAdvertisementData() {
-		return new ArrayData(
-			array(
-				"Hyperlink" => $this->Hyperlink,
-				"Logo" => $this->Logo()
-			)
-		);
-	}
-
+    public function WidgetAdvertisementData()
+    {
+        return new ArrayData(
+            array(
+                "Hyperlink" => $this->Hyperlink,
+                "Logo" => $this->Logo()
+            )
+        );
+    }
 }
--- _config.php
+++ _config.php
@@ -8,7 +8,3 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START widgets_widgetadvertisement MODULE ----------------===================
 //===================---------------- END widgets_widgetadvertisement MODULE ----------------===================
-
-
-
-
